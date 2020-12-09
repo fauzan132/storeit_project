04b8e13d1452c03962cdc5f5674e7ab314faf67e
Fauzan Arrasyid <falcon.1st.eye@gmail.com>
2020-12-09 17:59:21 +0700

    Route

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -133,7 +133,7 @@ desired effect
       </div>
       <!-- Sidebar Menu -->
       <ul class="sidebar-menu">
-        <li class="header">HEADER</li>
+        <li class="header">Menu</li>
         <!-- Optionally, you can add icons to the links -->
         <li class="active"><a href="#"><i class="fa fa-link"></i> <span>Link</span></a></li>
         <li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>
