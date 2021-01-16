bda5f4797386f9504c960537140622d9a75468c7
Fauzan Arrasyid <falcon.1st.eye@gmail.com>
2021-01-16 23:04:21 +0700

    menambahkan status pada list data

--- resources/views/tanaman/tanaman-data/list_data_all.blade.php
+++ resources/views/tanaman/tanaman-data/list_data_all.blade.php
@@ -37,7 +37,7 @@ img {
         <section class="col-lg-12 connectedSortable">
         <div class="box">
             <div class="box-header">
-              <h3 class="box-title">List Data Tanaman</h3>
+              <h3 class="box-title">List Data Tanaman Semua Pengguna</h3>
             </div>
             
             <!-- /.box-header -->
@@ -73,6 +73,7 @@ img {
                     <th>Owner</th>
                     <th>Plant Type</th>
                     <th>Waktu Upload</th>
+                    <th>Status</th>
                     <th>Crop Data</th>     
                     <th>Lihat Metadata</th>
                     <th>Hapus Data</th>
@@ -90,6 +91,7 @@ img {
                     <td>{{ $value->name }} - {{ $value->role }}</td>
                     <td>{{ $value->plantType }}</td>
                     <td>{{ $value->created_at }}</td>
+                    <td>{{ $value->status }}</td>
                     <td>
                         <a href="{{ url('tanaman-data/crop/awal_all', $value->imageID) }}" class="btn bg-olive btn-flat" title="Lihat Hasil Crop Data ini"><i class="fa fa-eye"></i> Lihat Data Crop</a><br><br>
                         <a href="{{ url('tanaman-data/cropping_all', $value->imageID) }}" class="btn btn-primary btn-flat" title="Crop Data ini"><i class="fa fa-crop"></i> Crop Gambar</a>
@@ -110,6 +112,7 @@ img {
                     <th>Owner</th>
                     <th>Plant Type</th>
                     <th>Waktu Upload</th>
+                    <th>Status</th>
                     <th>Crop Data</th>
                     <th>Lihat Metadata</th>
                     <th>Hapus Data</th>
