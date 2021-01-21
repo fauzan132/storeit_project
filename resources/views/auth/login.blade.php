c1103195d2f79804969fb5aa38aceed572c85955
Fauzan Arrasyid <falcon.1st.eye@gmail.com>
2021-01-22 02:32:42 +0700

    change logo

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -29,7 +29,7 @@
 		<div class="container-login100">
 			<div class="wrap-login100">
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="{{ asset('login_assets/images/logo.png') }}" alt="IMG">
+					<img src="{{ asset('images/logo_app.png') }}" alt="IMG" width="600px">
 				</div>
                 @if (session('status'))
                 <div class="alert alert-danger" role="alert">
